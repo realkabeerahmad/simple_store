0cb3b87da441cfc953ea290a501e74591180fc18
realkabeerahmad <kabeerahmadofficial@gmail.com>
2022-06-18 17:36:19 +0500

    added 3 products through db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create( name: "Huawei P-Smart",
+                description: "Only Few flagships in budget by Huawei",
+                price: 79.9
+              )
+Product.create( name: "Huawei P-30",
+                description: "Only Few flagships in Premium Quality by Huawei",
+                price: 79.9
+              )
+Product.create( name: "Huawei P-Keypad",
+                description: "Many flagships in low budget by Huawei",
+                price: 79.9
+              )
